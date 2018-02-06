e456992f24abf8a9d8e069059d506dfc3e03f70d
Mary Prier <priertroyandmary@gmail.com>
2018-02-06 19:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`
   }
+  
 function happyHolidayTo(holiday,name) {
     return `Happy Independence Day, You!`
 }
+
 function holidayCountdown(Independence Day,120) {
     return `It's ${days} days until ${holiday}!`
 }
